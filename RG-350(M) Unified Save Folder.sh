5dd34bdd6a67d24016ccf5ddb7fef23f948eade3
XargonWan <XargonWan@gmail.com>
2020-06-20 17:43:08 +0200

    Added Template and Genesis Plus GX
    Added the template commands and added support for Sega Mega Drive (Genesis),Mega CD, Master System, Game Gear & SG-1000 - Genesis Plus GX.

--- RG-350(M) Unified Save Folder.sh
+++ RG-350(M) Unified Save Folder.sh
@@ -6,9 +6,55 @@
 #- Default directories -
 #roms: /media/RG-350/roms
 #saves/states: /media/RG-350/saves
+#bios: 
+
+#Template
+#saves
+#mv /media/data/local/home/.FOO /media/data/local/home/.FOO_bak
+#ln -s /media/RG-350/saves /media/data/local/home/.FOO
+#mv  /media/data/local/home/.FOO_bak/* /media/data/local/home/.FOO/
+#rm /media/data/local/home/.FOO_bak
+#bios
+#mv /media/data/local/home/.FOO /media/data/local/home/.FOO_bak
+#ln -s /media/RG-350/saves /media/data/local/home/.FOO
+#mv  /media/data/local/home/.FOO_bak/* /media/data/local/home/.FOO/
+#rm /media/data/local/home/.FOO_bak
 
 #Game Boy Advance - ReGBA
 mv /media/data/local/home/.gpsp /media/data/local/home/.gpsp_bak
 ln -s /media/RG-350/saves /media/data/local/home/.gpsp
 mv  /media/data/local/home/.gpsp_bak/* /media/data/local/home/.gpsp/
 rm /media/data/local/home/.gpsp_bak
+#Seems like there is no BIOS folder
+
+#Sega Mega Drive (Genesis),Mega CD, Master System, Game Gear & SG-1000 - Genesis Plus GX
+#saves cd
+mv /media/data/local/home/.genplus/saves/cd /media/data/local/home/.genplus/saves/cd_bak
+ln -s /media/RG-350/saves /media/data/local/home/.genplus/saves/cd
+mv  /media/data/local/home/.genplus/saves/cd_bak/* /media/data/local/home/.genplus/saves/cd
+rm /media/data/local/home/.genplus/saves/cd_bak
+#saves gg
+mv /media/data/local/home/.genplus/saves/gg /media/data/local/home/.genplus/saves/gg_bak
+ln -s /media/RG-350/saves /media/data/local/home/.genplus/saves/gg
+mv  /media/data/local/home/.genplus/saves/gg_bak/* /media/data/local/home/.genplus/saves/gg
+rm /media/data/local/home/.genplus/saves/gg_bak
+#saves md
+mv /media/data/local/home/.genplus/saves/md /media/data/local/home/.genplus/saves/md_bak
+ln -s /media/RG-350/saves /media/data/local/home/.genplus/saves/md
+mv  /media/data/local/home/.genplus/saves/md_bak/* /media/data/local/home/.genplus/saves/md
+rm /media/data/local/home/.genplus/saves/md_bak
+#saves ms
+mv /media/data/local/home/.genplus/saves/ms /media/data/local/home/.genplus/saves/ms_bak
+ln -s /media/RG-350/saves /media/data/local/home/.genplus/saves/ms
+mv  /media/data/local/home/.genplus/saves/ms_bak/* /media/data/local/home/.genplus/saves/ms
+rm /media/data/local/home/.genplus/saves/ms_bak
+#saves sg
+mv /media/data/local/home/.genplus/saves/sg /media/data/local/home/.genplus/saves/sg_bak
+ln -s /media/RG-350/saves /media/data/local/home/.genplus/saves/sg
+mv  /media/data/local/home/.genplus/saves/sg_bak/* /media/data/local/home/.genplus/saves/sg
+rm /media/data/local/home/.genplus/saves/sg_bak
+#bios
+mv /media/data/local/home/.genplus/bios /media/data/local/home/.genplus/bios_bak
+ln -s /media/RG-350/saves /media/data/local/home/.genplus/bios
+mv  /media/data/local/home/.genplus/bios_bak/* /media/data/local/home/.genplus/bios
+rm /media/data/local/home/.genplus/bios_bak
