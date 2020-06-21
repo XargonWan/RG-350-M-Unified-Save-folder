b8a01e836f702c4ac06463c06db3cf038f56896f
XargonWan <XargonWan@gmail.com>
2020-06-21 20:12:52 +0200

    Update RG-350(M) Unified Save Folder.sh
    - Fixed Genesis Plus GX
    - Modified variable names, makes more sense now

--- RG-350(M) Unified Save Folder.sh
+++ RG-350(M) Unified Save Folder.sh
@@ -4,17 +4,17 @@
 #The SD Card must be renamed into RG-350, otherwise you have to fix the script.
 
 #Adding global variables for the default directories
-save_folder=/media/RG-350/saves      #Golbal Save Folder
-bios_folder=/media/RG-350/bios       #Global BIOS Folder
+global_savefolder=/media/RG-350/saves      #Golbal Save Folder
+global_biosfolder=/media/RG-350/bios       #Global BIOS Folder
 
 #Template with description
 #Platform Name - Emulator name
 
-#Original_Folder=/media/data/local/home/.FOO	#initalizing saves folder
-#mv $Original_Folder $Original_Folder"_bak"		#renaming the folder in the folder_bak
-#ln -s $save_folder $Original_Folder 			#creating a symbolic link with the name of the original folder pointing at the gloabl save folder
-#mv $Original_Folder"_bak"/* $Original_Folder	#moving all the files inside the backed up folder into the newly created folder, actually inside the golbal save folder
-#rm $Original_Folder"_bak"						#deleting the old backup folder
+#original_folder=/media/data/local/home/.FOO	#initalizing saves folder [don't put / in the end]
+#mv $original_folder $original_folder"_bak"		#renaming the folder in the folder_bak
+#ln -s $global_savefolder $original_folder 			#creating a symbolic link with the name of the original folder pointing at the gloabl save folder
+#mv $original_folder"_bak"/* $original_folder	#moving all the files inside the backed up folder into the newly created folder, actually inside the golbal save folder
+#rm $original_folder"_bak"						#deleting the old backup folder
 
 ### That's enough gabbing, the script is starting.
 
@@ -24,58 +24,60 @@ bios_folder=/media/RG-350/bios       #Global BIOS Folder
 #GBC: gambatte.opk
 
 #saves
-Original_Folder=/media/data/local/home/.gambatte/saves	#initalizing saves folder
-mv $Original_Folder $Original_Folder"_bak"				#renaming the folder in the folder_bak
-ln -s $save_folder $Original_Folder 					#creating a symbolic link with the name of the original folder pointing at the gloabl save folder
-mv $Original_Folder"_bak"/* $Original_Folder			#moving all the files inside the backed up folder into the newly created folder, actually inside the golbal save folder
-rm $Original_Folder"_bak"								#deleting the old backup folder
+original_folder=/media/data/local/home/.gambatte/saves	#initalizing saves folder
+mv $original_folder $original_folder"_bak"				#renaming the folder in the folder_bak
+ln -s $global_savefolder $original_folder 					#creating a symbolic link with the name of the original folder pointing at the gloabl save folder
+mv $original_folder"_bak"/* $original_folder			#moving all the files inside the backed up folder into the newly created folder, actually inside the golbal save folder
+rm $original_folder"_bak"								#deleting the old backup folder
 
 #bios
 original_bios=/media/data/local/home/.gambatte/bios		#initalizing bios folder
 mv $original_bios $original_bios"_bak"					#renaming the folder in the folder_bak
-ln -s $bios_folder $original_bios 						#creating a symbolic link with the name of the original folder pointing at the gloabl bios folder
+ln -s $global_biosfolder $original_bios 						#creating a symbolic link with the name of the original folder pointing at the gloabl bios folder
 mv $original_bios"_bak"/* $original_bios					#moving all the files inside the backed up folder into the newly created folder, actually inside the golbal bios folder
 rm $original_bios"_bak"									#deleting the old backup folder
 
 #Game Boy Advance - ReGBA
-Original_Folder=/media/data/local/home/.gpsp			#initalizing saves folder
-mv $Original_Folder $Original_Folder"_bak"				#renaming the folder in the folder_bak
-ln -s $save_folder $Original_Folder 					#creating a symbolic link with the name of the original folder pointing at the gloabl save folder
-mv $Original_Folder"_bak"/* $Original_Folder			#moving all the files inside the backed up folder into the newly created folder, actually inside the golbal save folder
-rm $Original_Folder"_bak"								#deleting the old backup folder
+original_folder=/media/data/local/home/.gpsp			#initalizing saves folder
+mv $original_folder $original_folder"_bak"				#renaming the folder in the folder_bak
+ln -s $global_savefolder $original_folder 					#creating a symbolic link with the name of the original folder pointing at the gloabl save folder
+mv $original_folder"_bak"/* $original_folder			#moving all the files inside the backed up folder into the newly created folder, actually inside the golbal save folder
+rm $original_folder"_bak"								#deleting the old backup folder
 #Changing file extension from .srm to .sav, ReGBA is not supporting .srm files
-cd $save_folder
+cd $global_savefolder
 for x in *.srm; do mv "$x" "${x%.srm}.sav"; done
 #Seems like there is no BIOS folder
 
 #Sega Mega Drive (Genesis),Mega CD, Master System, Game Gear & SG-1000 - Genesis Plus GX
+original_folder=/media/data/local/home/.genplus/saves
 #saves cd
-mv /media/data/local/home/.genplus/saves/cd /media/data/local/home/.genplus/saves/cd_bak
-ln -s /media/RG-350/saves /media/data/local/home/.genplus/saves/cd
-mv  /media/data/local/home/.genplus/saves/cd_bak/* /media/data/local/home/.genplus/saves/cd
-rm /media/data/local/home/.genplus/saves/cd_bak
+mv $original_folder"/cd" $original_folder"/cd_bak"
+ln -s $global_savefolder $original_folder"/cd"
+mv  $original_folder"/cd_bak"/* $original_folder"/cd"
+rm $original_folder"/cd_bak"
 #saves gg
-mv /media/data/local/home/.genplus/saves/gg /media/data/local/home/.genplus/saves/gg_bak
-ln -s /media/RG-350/saves /media/data/local/home/.genplus/saves/gg
-mv  /media/data/local/home/.genplus/saves/gg_bak/* /media/data/local/home/.genplus/saves/gg
-rm /media/data/local/home/.genplus/saves/gg_bak
+mv $original_folder"/gg" $original_folder"/gg_bak"
+ln -s $global_savefolder $original_folder"/gg"
+mv  $original_folder"/gg_bak"/* $original_folder"/gg"
+rm $original_folder"/gg_bak"
 #saves md
-mv /media/data/local/home/.genplus/saves/md /media/data/local/home/.genplus/saves/md_bak
-ln -s /media/RG-350/saves /media/data/local/home/.genplus/saves/md
-mv  /media/data/local/home/.genplus/saves/md_bak/* /media/data/local/home/.genplus/saves/md
-rm /media/data/local/home/.genplus/saves/md_bak
+mv $original_folder"/md" $original_folder"/md_bak"
+ln -s $global_savefolder $original_folder"/md"
+mv  $original_folder"/md_bak"/* $original_folder"/md"
+rm $original_folder"/md_bak"
 #saves ms
-mv /media/data/local/home/.genplus/saves/ms /media/data/local/home/.genplus/saves/ms_bak
-ln -s /media/RG-350/saves /media/data/local/home/.genplus/saves/ms
-mv  /media/data/local/home/.genplus/saves/ms_bak/* /media/data/local/home/.genplus/saves/ms
-rm /media/data/local/home/.genplus/saves/ms_bak
+mv $original_folder"/ms" $original_folder"/ms_bak"
+ln -s $global_savefolder $original_folder"/ms"
+mv  $original_folder"/ms_bak"/* $original_folder"/ms"
+rm $original_folder"/ms_bak"
 #saves sg
-mv /media/data/local/home/.genplus/saves/sg /media/data/local/home/.genplus/saves/sg_bak
-ln -s /media/RG-350/saves /media/data/local/home/.genplus/saves/sg
-mv  /media/data/local/home/.genplus/saves/sg_bak/* /media/data/local/home/.genplus/saves/sg
-rm /media/data/local/home/.genplus/saves/sg_bak
+mv $original_folder"/sg" $original_folder""/sg"_bak"
+ln -s $global_savefolder $original_folder"/sg"
+mv  $original_folder"/sg_bak"/* $original_folder"/sg"
+rm $original_folder"/sg_bak"
 #bios
-mv /media/data/local/home/.genplus/bios /media/data/local/home/.genplus/bios_bak
-ln -s /media/RG-350/saves /media/data/local/home/.genplus/bios
-mv  /media/data/local/home/.genplus/bios_bak/* /media/data/local/home/.genplus/bios
-rm /media/data/local/home/.genplus/bios_bak
+original_folder=/media/data/local/home/.genplus/bios
+mv $original_folder $original_folder"_bak"
+ln -s $global_biosfolder $original_folder
+mv  $original_folder"_bak"/* $original_folder
+rm $original_folder"_bak"
