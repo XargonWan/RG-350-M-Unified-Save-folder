41263eb40f387267fad900874dc5278378674d36
XargonWan <XargonWan@gmail.com>
2020-06-26 15:18:58 +0200

    Update RG-350(M) Unified Save Folder.sh

--- RG-350(M) Unified Save Folder.sh
+++ RG-350(M) Unified Save Folder.sh
@@ -104,8 +104,8 @@ rm $original_folder"_bak"						#deleting the old backup folder
 #Tomb Raider I - OpenLara
 #Template v0.3
 
-#original_folder=/media/data/local/home/.openlara	#initalizing saves folder [don't put / in the end]
-#mv $original_folder $original_folder"_bak"		#renaming the folder in the folder_bak
-#ln -s $global_savefolder $original_folder 			#creating a symbolic link with the name of the original folder pointing at the gloabl save folder
-#mv $original_folder"_bak"/* $original_folder	#moving all the files inside the backed up folder into the newly created folder, actually inside the golbal save folder
-#rm $original_folder"_bak"						#deleting the old backup folder
+original_folder=/media/data/local/home/.openlara	#initalizing saves folder [don't put / in the end]
+mv $original_folder $original_folder"_bak"		#renaming the folder in the folder_bak
+ln -s $global_savefolder $original_folder 			#creating a symbolic link with the name of the original folder pointing at the gloabl save folder
+mv $original_folder"_bak"/* $original_folder	#moving all the files inside the backed up folder into the newly created folder, actually inside the golbal save folder
+rm $original_folder"_bak"						#deleting the old backup folder
